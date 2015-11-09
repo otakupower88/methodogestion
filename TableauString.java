31cc419df91011ad1be9d71fce6a74d5b8d2caac
Gaetan LACROIS <11401107@r10115.iutv.univ-paris13.fr>
2015-11-09 18:02:55 +0100

    premier commit gaetan

--- TableauString.java
+++ TableauString.java
@@ -1,4 +1,36 @@
-
 public class TableauString {
 
+	int rechercheDicho(String tab1[], String val) {
+
+		boolean trouve;
+		int id;
+		int ifin;
+		int im;
+
+		trouve = false;
+		id = 0;
+		ifin = tab1.length;
+
+		while (!trouve && ((ifin - id) > 1)) {
+
+			im = (id + ifin) / 2;
+
+			trouve = (tab1[im].compareTo(val) == 0);
+
+			if (tab1[im].compareTo(val) > 0) {
+				ifin = im;
+			} else {
+				id = im;
+			}
+		}
+		
+		if (tab1[id].compareTo(val) == 0) {
+			System.out.println("la valeur est dans le tableau à l'indice: "
+					+ id);
+		} else {
+			System.out.println("la valeur n'est pas dans le tableau ");
+
+		}
+		return 0;
+	}
 }
---
+++ Test.java
@@ -0,0 +1,16 @@
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TableauString a = new TableauString();
+		String tab1[] = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+				"k", "l", "m" };
+		String c = "c";
+		a.rechercheDicho(tab1, c);
+
+	}
+
+}
