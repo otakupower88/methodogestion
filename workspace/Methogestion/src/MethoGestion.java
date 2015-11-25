39c3652609cd6749074ef7d536cf4f82207829d5
otakupower88 <paul.marin-martinod@orange.fr>
2015-11-25 10:01:32 +0100

    tableau entier 2

---
+++ workspace/Methogestion/src/MethoGestion.java
@@ -0,0 +1,13 @@
+
+public class MethoGestion {
+
+	public static void main(String[] args) {
+		int taille = (int)(Math.random()*10);
+		int[] tab = new int[taille];
+		TableauEntier a=new TableauEntier(tab,tab.length);
+		a.affichageTab(a.getTab(), a.getTaille(a.getTab()));
+		a.tabRecherche(a.getTab(), a.getTaille(a.getTab()),5);
+
+	}
+
+}
